4af9befb0a503095223d41242407a930f0a3ef8d
Bruno Borges Russian <bruno.russian.i@gmail.com>
2021-10-20 16:51:06 -0300

    Add PingView POST method

--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,10 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
 from django.views.generic import View
 
 
 class PingView(View):
     def get(self, request, *args, **kwargs):
-        return HttpResponse("pong")
+        return JsonResponse({"pong": True})
+
+    def post(self, request, *args, **kwargs):
+        return JsonResponse({"pong": True})
